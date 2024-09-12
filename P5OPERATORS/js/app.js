c0377e47a480498012cbd1db15751f562fc6f067
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-12 14:22:57 +0200

    hi

--- P5OPERATORS/js/app.js
+++ P5OPERATORS/js/app.js
@@ -1,12 +1,7 @@
 
+let Numbersofstudents:number = 10;
 
-let firstNumber:number=10;
-let secondNumber:number=20;
-let answer:number=0;
-//1+2=3
+Numbersofstudents = +1;
 
-answer = firstNumber + secondNumber;
-console.log(answer);
 
-answer = firstNumber + secondNumber;
-console.log(answer);
+console.log(Numbersofstudents)
