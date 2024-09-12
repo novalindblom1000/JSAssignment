56fe0cedb74daca789c8cf67e2b5f06d1ea01491
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-12 10:55:45 +0200

    hi

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -3,3 +3,16 @@ console.log("Hello World!");
 console.log("First.");
 console.log("Second.");
 
+const five:number=5;
+const eight:number=8;
+
+console.log(five)
+console.log(eight)
+total=five+eight;
+
+
+// This is a single-line comment.
+
+/* ok,int y = 5;it hello ends only here */
+
+
