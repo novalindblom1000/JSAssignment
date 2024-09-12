c6e15b27e5766b2e4781b979fee9df79efbe0b08
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-12 11:40:16 +0200

    hi

---
+++ p4variables/js/app.js
@@ -0,0 +1,26 @@
+
+
+//INITIAL VARIABLES
+let numberOfBottle=10;
+const line1 = "green bottles hanging on the wall";
+const line2="and if one green bottle should accidentally fall, there will be";
+
+//OUTPUT
+
+console.log(line1)
+console.log(line2)
+console.log(numberOfBottle)
+console.log(line1)
+console.log(line2)
+console.log(numberOfBottle)
+
+
+console.log(line1);
+console.log(line2);
+console.log(--numberOfBottle)
+
+
+
+//Loops streamline repetitive tasks, reduce redundancy, and enhance maintainability.
+
+
