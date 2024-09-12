4515af43eb351cad9fa95222786304e930dd1012
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-12 14:54:00 +0200

    Create app.js

---
+++ P5Operators task/app.js
@@ -0,0 +1,13 @@
+let Speedinput=72;
+let Speedoutput:number= 20;
+Speedoutput = Speedinput/3.6
+console.log(Speedoutput)
+
+//minutes input = seconds.
+
+let minutesInput =0
+let secondsOutput:number = 0
+
+  minutesInput= secondsOutput *60
+console.log(minutesInput)
+
