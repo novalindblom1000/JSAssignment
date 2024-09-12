33ba135773fbf864781f767d25f3e104ab95dc1c
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-12 15:29:50 +0200

    yeah

--- P5Operators task/app.js
+++ P5Operators task/app.js
@@ -1,15 +1,24 @@
-let Speedinput=72;
-let Speedoutput= 20;
+let Speedinput=72; //KM/h = M/s
+let Speedoutput=20;
 Speedoutput = Speedinput/3.6;
 console.log(Speedoutput);
-
-/**
- * minutes input = seconds.
- */
 
 let minutesInput =3;
 let secondsOutput= 180;
 
-  minutesInput= secondsOutput *60;
+minutesInput= secondsOutput *60; //Minutes to seconds
 console.log (minutesInput);
+
+let Onenumberinput= 11;
+let Twonumberinput=4;
+let Divisioninput1;
+let Divisioninput2;
+
+Divisioninput1 = Onenumberinput/2.75;
+Divisioninput2 = Twonumberinput/2.75;
+
+console.log(Divisioninput1);
+console.log(Divisioninput2);
+
+
 
