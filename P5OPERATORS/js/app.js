b75da898d9ac12ad489e9aafacf14049df6d3fdc
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-12 12:39:48 +0200

    ye

--- P5OPERATORS/js/app.js
+++ P5OPERATORS/js/app.js
@@ -1,13 +1,10 @@
-let x = 5 + 5; // x = 10
-let y = x + 5; // y = 15
-let z = y + y; // z = 30
-console.log(z + z); // 60
+
+
 
 let One:number= 1;
 let Two:number= 2;
+Answer= One + Two;
 
-Answer= One + Two
-
-console.log(Answer)
+(console.log(Answer);
 
 
