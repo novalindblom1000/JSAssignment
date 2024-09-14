51b84af02154caed01d5b415670c7a0c371764b5
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-14 16:36:15 +0200

    j

--- P5OPERATORS/js/app.js
+++ P5OPERATORS/js/app.js
@@ -16,3 +16,13 @@ console.log(answer);
 
 answer = firstnumber % secondnumber;
 console.log(answer);
+
+Kilometerperhour =72;
+Secondsperminute = 0;
+
+Secondsperminute = Kilometerperhour /3.6;
+console.log(Secondsperminute);
+
+Minutesnumber=3;
+Secondsdisplay=0;
+
