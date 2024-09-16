ae5c8d5fae744eba5ccae730421b3598c9a186a2
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-16 14:40:18 +0200

    Update app.js

--- Functions/js/app.js
+++ Functions/js/app.js
@@ -2,3 +2,18 @@
 console.log("Move Forward.");
 
 console.log("Turn Right.");
+
+function step () {
+console.log("Move Forward.");
+}
+
+function turnRight() {
+  console.log("Turn Right.");
+}
+
+function turnleft () {
+  turnRight()
+  turnRight()
+  turnRight()
+}
+//a function connected to previous function so will actually turn left cause can't 
