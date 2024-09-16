bbaacf3cd878d7519376b194582e9c4735140f07
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-16 13:31:40 +0200

    j

--- Boolean P7/js/app.js
+++ Boolean P7/js/app.js
@@ -3,14 +3,29 @@ const currentYear = 2024;
 let ageOfPerson = currentYear - birthYear;
 console.log(ageOfPerson);
 
+
 // Save to a let variable named isChild, when the age is from 0 till 10.
 let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
-
 // Save to a let variable named isTeenager, when the age is from 11 to 17
 let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
-
 // Save to a let variable named isAdult, when the age is 18 or more.
 let isAdult =  ageOfPerson >= 18;
 
+if (isChild) {
 
+console.log ("kindgardener");
+
+}
+
+if (isTeenager) {
+
+  console.log ("Teen rebbelion");
+
+}
+
+if(isAdult) {
+
+  console.log ("Adult years");
+
+}
 
