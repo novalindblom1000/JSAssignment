4c9ddd8f4eb8f17bf76688bc8a24c0c7c857d917
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-16 11:00:18 +0200

    hi

--- Boolean P7/js/app.js
+++ Boolean P7/js/app.js
@@ -2,8 +2,8 @@ const birthYear=2000;
 const currentyear=2024;
 let ageOfperson = currentyear-birthYear;
 
-let isChild=ageOfperson <=11
-let isTeen=ageOfperson <=17
+let isChild=ageOfperson <=0;
+let isTeen=ageOfperson <=11;
 
 let isAdult= ageOfperson >=18;
 console.log(isAdult);
