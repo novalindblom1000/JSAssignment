f22fa0712db62bce0201aba056ce35ac8cda49b0
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-17 09:58:32 +0200

    j

--- FUNCTIONSINPUT &OUTPUT/js/app.js
+++ FUNCTIONSINPUT &OUTPUT/js/app.js
@@ -2,8 +2,19 @@
 
 function multiplyBySelf (Number2multiply){
 
-return Number2multiply & Number2multiply
+return Number2multiply * Number2multiply;
 
 
 }
-console.log(Number2multiply)
+console.log(multiplyBySelf(4));
+
+function add (firstNumber, secondNumber){
+  return firstNumber + secondNumber;
+}
+
+console.log(add(5,20));
+
+function add(Ben, Ting){
+  return ben + ting;
+
+  console.log(add("Welcome Ben Ting!"));
