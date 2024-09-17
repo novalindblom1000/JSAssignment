7f53d3875a640c96a3b1629e13ecd9f9bfcbcceb
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-17 10:47:31 +0200

    j

--- FUNCTIONSINPUT &OUTPUT/js/app.js
+++ FUNCTIONSINPUT &OUTPUT/js/app.js
@@ -14,14 +14,22 @@ function add (firstNumber, secondNumber){
 
 console.log(add(5,20));
 
-function add(Ben, Ting){
-  return ben + ting;
-
-  console.log(add("Welcome Ben Ting!"));
-
-  
   function welcomeUsers(name1,name2){
-    return'welcome'+name1+name2;
+    return'Welcome '+ name1 + ' '+  name2 + '!';
   }
 
-console.log(welcomeUsers(Ben,Ting));
+console.log(welcomeUsers('Ben','Ting'));
+
+  function todoLIST (item1,item2,item3) {
+    return '20' +' '+ item1 +' '+'10' +' '+ item2 +' '+ '2' +' '+ item3;
+  }
+
+  console.log(todoLIST('milk','cereal','crackers'));
+
+
+function oneName(name3='name'){
+  return name3;
+}
+console.log(oneName());
+
+
