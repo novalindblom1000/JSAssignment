1528d42d088baf38ef852c79bb8761d88d2215a3
novalindblom1000 <novlin601@ihmeducation.se>
2024-09-17 14:02:48 +0200

    j

---  P13HTMLDOM/js/app.js
+++  P13HTMLDOM/js/app.js
@@ -4,4 +4,21 @@
 
 const button0 = document.getElementById("button0");
 
-button0.addEventListener("click", () => {yourFunctionHere();});
+button0.addEventListener("click", () => {increaseBy();});
+
+let score;
+function increaseBy(inputNumber){
+score
+}
+function  updateScore(){
+  "text"
+  score
+
+}
+
+increaseBy(1);
+updateScore();
+
+
+//button id="button0"> yesss /button> -old stuff should place in html.
+
